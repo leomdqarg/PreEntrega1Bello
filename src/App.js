f5df8bbccd50bc2037398ea1b750d2181f4036cb
Leonardo Bello <leomdqarg@gmail.com>
2022-10-01 15:24:05 -0300

    agregue archivos necesarios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import NavBar from './components/Navbar/Navbar.js'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.js'
+import ItemCount from './components/ItemCount/ItemCount.js'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer.js'
 import './App.css'
 import 'bootstrap'
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div id="App">
       <NavBar />
       <ItemListContainer greetings={`próximamente listado de los productos`} />
+      <ItemDetailContainer />
+      <ItemCount initial={10} stock={20} onAdd={() => {console.log('added')}} ></ItemCount>
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+const ItemListContainer = ({initial, stock, onAdd}) => {
+    const [counter, setCounter] = useState(initial)
+
+    const incrementCounter = () => {
+        console.log('incrementCounter', counter)
+        if (counter < stock)
+        {
+            setCounter(counter+1)
+        }
+    }
+
+    const decrementCounter = () => {
+        console.log('decrementCounter', counter)
+        if (counter > 0 )
+        {
+            setCounter(counter-1)
+        }
+    }
+
+    return (
+        <div>
+            <div className='alert alert-primary' role='alert'>{counter}</div>
+            <button onClick={incrementCounter}><FontAwesomeIcon icon={faPlusCircle} /></button>
+            <button onClick={decrementCounter}><FontAwesomeIcon icon={faMinus} /></button>
+            <button onClick={onAdd}>Agregar al carrito</button>
+        </div>
+    );
+  }
+
+export default ItemListContainer;
+
+
